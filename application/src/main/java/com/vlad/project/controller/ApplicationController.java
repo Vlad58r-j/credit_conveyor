7096c2ff6a8be209674c224887406b20001d7465
Vlad Krivonos <vlad58r@gmail.com>
2026-09-24 17:12:16 +0300

    Доработал application API
    Реализовал через Validation прескоринг данных в LoanApplicationRequestDto, добавил аннотацию Age и класс AgeValidation, для валидации возраста
    
    Написал конфиг и клиента для переадресации на другой микросервис - deal
    
    Реализовал сервис, который вызывает единственный метод из HttpDealClient, который возвращает 4 отсортированных получившихся предложения
    
    Добавил аннотации в дто для валидации
    
    Поправил контроллер: добавил аннотацию @Valid в параметры метода; в return добавил вызов метода из сервиса

--- application/src/main/java/com/vlad/project/controller/ApplicationController.java
+++ application/src/main/java/com/vlad/project/controller/ApplicationController.java
@@ -2,6 +2,9 @@ package com.vlad.project.controller;
 
 import com.vlad.project.dto.LoanApplicationRequestDto;
 import com.vlad.project.dto.LoanOfferDto;
+import com.vlad.project.service.ApplicationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,12 +15,16 @@ import java.util.List;
 @Slf4j
 @Controller
 @RequestMapping("/application")
+@RequiredArgsConstructor
 public class ApplicationController {
+
+    private final ApplicationService service;
 
     @PostMapping()
     public ResponseEntity<List<LoanOfferDto>> apllication(@RequestBody
+                                                          @Valid
                                                           LoanApplicationRequestDto requestDto) {
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(service.getApplicationFromMsConveyor(requestDto));
     }
 
     @PutMapping("/offer")
